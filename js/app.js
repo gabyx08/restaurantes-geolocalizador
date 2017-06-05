dfb759b18c71ed1651de50122e5f164601c8b9c0
fbk-rena <gaby.ranarz@gmail.com>
2017-06-05 16:16:54 -0500

    se muestra arreglo de restaurantes en el html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,61 @@
+var arregloRestaurantes = [
+  {
+    nombre: "Zapote",
+    direccion: "Calle Guanajuato 138, Cuauhtémoc, Roma Nte., 06700 Ciudad de México, CDMX",
+    latitud: 19.416378,
+    longitud: -99.16085,
+    comida: "mediterránea",
+    foto: "http://lorempixel.com/100/100/food/6"
+  },
+  {
+     nombre: "El Japonez",
+     direccion: "Av. Vicente Suarez 42A, Cuauhtemoc, Hipódromo Condesa, 06170 Ciudad de México, CDMX",
+     latitud: 19.4111767,
+     longitud: -99.1749857,
+     comida: "japonesa",
+     foto: "http://lorempixel.com/100/100/food/6"
+   },
+   {
+     nombre: "Fonda Mexicana",
+     direccion: "Avenida Montevideo 279, Gustavo A. Madero, Lindavista",
+     latitud: 19.4901029,
+     longitud: -99.1309569,
+     comida: "mexicana",
+     foto: "http://lorempixel.com/100/100/food/6"
+   },
+   {
+     nombre: "El Cardenal",
+     direccion: "Av. de la Paz Núm. 32, Alvaro Obregon, San Ángel, 01000 Ciudad de México, CDMX",
+     latitud: 19.34692,
+     longitud: -99.1887497,
+     comida: "mexicana",
+     foto: "http://lorempixel.com/100/100/food/6"
+   },
+   {
+    nombre: "Falafelito",
+    direccion: "Av. México 105, Hipódromo Condesa, 06100, CDMX.",
+    latitud: 19.4102471,
+    longitud:-99.1704605,
+    comida: "Vegana",
+    foto: "http://lorempixel.com/100/100/food/6"
+  }];
+
+   var plantillaDatosRestaurante = '<div class="card horizontal">'+
+     '<div class="card-image">'+
+       '<img src=_foto_>'+
+     '</div>'+
+     '<div class="card-stacked">'+
+       '<div class="card-content">'+
+         '<h5 class="header">_NombreRestaurante_</h2>'+
+         '<p>_direccion_</p>'+
+         '<p>Tipo de comida: _comida_</p>'+
+       '</div>'+
+     '</div>'+
+   '</div>';
+   //---------------------------------------------//
 var cargarPagina = function(){
   obtenerUbicacion();
+  mostrarRestaurantes();
 };
 var obtenerUbicacion = function(){
   if (navigator.geolocation) {
@@ -9,7 +65,7 @@ var obtenerUbicacion = function(){
 	}
 }
 var mostrarPosicion = function (posicion) {
-	// console.log(posicion);
+	//console.log(posicion);
 	var coordenadas = {
 		lat: posicion.coords.latitude,
 		lng: posicion.coords.longitude
@@ -27,4 +83,16 @@ var mostrarMapa = function (coordenadas) {
       map: map
     });
 }
+
+var mostrarRestaurantes= function () {
+	var plantillaFinal = "";
+	arregloRestaurantes.forEach(function (restaurante) {
+		plantillaFinal += plantillaDatosRestaurante.replace("_NombreRestaurante_", restaurante.nombre)
+      .replace("_comida_", restaurante.comida)
+			.replace("_direccion_", restaurante.direccion)
+			.replace("_foto_", restaurante.foto);
+	});
+	$("#restaurantes").html(plantillaFinal);
+};
+
 $(document).ready(cargarPagina);
